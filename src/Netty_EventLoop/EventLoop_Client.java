1307f559b8c72abf8683e9b827ad78379d90ea50
WangYiiiiF <wangyiiiif@163.com>
2023-09-14 13:17:46 +0800

    Initial commit

---
+++ src/Netty_EventLoop/EventLoop_Client.java
@@ -0,0 +1,37 @@
+package Netty_EventLoop;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class EventLoop_Client {
+    public static void main(String[] args) throws InterruptedException {
+        // 1. 启动类
+        Channel channel = new Bootstrap()
+                // 2. 添加 EventLoop
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel();
+
+        channel.writeAndFlush("TS");
+        Thread.sleep(2000);
+        System.out.println(channel);
+        System.out.println("");
+    }
+}
